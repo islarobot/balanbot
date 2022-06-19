716b812b18ee766cc310ab97fdc3519295e16864
islarobot <islarobots@gmail.com>
2022-06-19 22:14:49 +0200

    s

--- balanbot_emisor_pruebas_arduino/balanbot_emisor_pruebas_arduino.ino
+++ balanbot_emisor_pruebas_arduino/balanbot_emisor_pruebas_arduino.ino
@@ -15,14 +15,8 @@ void loop() {
   if (Serial.available()) {
     c=Serial.read();
     delay(10);
-    byte check = speed^0x01^0x01;
+    mySerial.write(d);
 
-    mySerial.write(0xAA);
-    mySerial.write(0x03);
-    mySerial.write(0x01);
-    mySerial.write(0x01);
-    mySerial.write(speed);
-    mySerial.write(check);
   }
   delay(10);
   if (mySerial.available()) {
